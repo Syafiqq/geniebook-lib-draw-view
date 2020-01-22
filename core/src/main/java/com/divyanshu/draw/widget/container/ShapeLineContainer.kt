30130c35baf35c3a88c34d7cb18b01f117b09661
Syafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2020-01-22 16:29:33 +0700

    add shape line container

---
+++ core/src/main/java/com/divyanshu/draw/widget/container/ShapeLineContainer.kt
@@ -0,0 +1,119 @@
+package com.divyanshu.draw.widget.container
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.MotionEvent
+import androidx.annotation.ColorInt
+import androidx.core.graphics.ColorUtils
+import com.divyanshu.draw.widget.contract.*
+import com.divyanshu.draw.widget.mode.LineMode
+
+class ShapeLineContainer(override val context: Context, override val drawing: ICanvas) : IDrawingContainer<LineMode>, IPaint {
+    override var draw: LineMode? = null
+
+    private val listener: InteractionListener
+
+    private var _color = 0
+    private var _strokeWidth = 0F
+    private var _alpha = 0
+
+    private val paint = Paint()
+
+    override var color: Int
+        get() = _color
+        set(value) {
+            @ColorInt
+            val alphaColor = ColorUtils.setAlphaComponent(value, alpha)
+            _color = alphaColor
+            draw?.color = alphaColor
+        }
+    override var strokeWidth: Float
+        get() = _strokeWidth
+        set(value) {
+            _strokeWidth = value
+            draw?.strokeWidth = value
+        }
+    override var alpha: Int
+        get() = _alpha
+        set(value) {
+            _alpha = (value * 255) / 100
+            color = color
+        }
+    override var textSize = 0F
+
+    init {
+        val ctx = this.context
+        if (ctx !is InteractionListener) {
+            throw ClassCastException("context must implement InteractionListener")
+        }
+
+        listener = ctx
+        with(paint) {
+            style = Paint.Style.STROKE
+            strokeJoin = Paint.Join.ROUND
+            strokeCap = Paint.Cap.ROUND
+        }
+    }
+
+    override fun onDraw(canvas: Canvas, draw: IMode) {
+        if (draw !is LineMode) return
+        draw.onDraw(canvas, paint)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        draw?.let { onDraw(canvas, it) }
+    }
+
+    override fun createDrawingObject(x: Float, y: Float, event: MotionEvent) {
+        if (draw != null || event.pointerCount > 1) return
+
+        attachDrawingTool()
+        draw = LineMode(DrawingMode.LINE).apply {
+            color = this@ShapeLineContainer.color
+            strokeWidth = this@ShapeLineContainer.strokeWidth
+            onFingerDown(x, y)
+        }
+        drawing.requestInvalidate()
+    }
+
+    override fun attachDrawingTool() {
+        listener.attachComponent(this)
+    }
+
+    override fun detachDrawingTool() {
+        listener.detachComponent()
+    }
+
+    override fun destroyDrawingObject() {
+        draw = null
+        detachDrawingTool()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (draw == null) return true
+        if (event.pointerCount > 1) {
+            destroyDrawingObject()
+            return true
+        }
+
+        val x = event.x
+        val y = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_MOVE -> draw?.onFingerMove(x, y)
+            MotionEvent.ACTION_UP -> {
+                draw?.onFingerUp(x, y)
+                draw?.let(drawing::attachToCanvas)
+            }
+        }
+
+        drawing.requestInvalidate()
+        return true
+    }
+
+    interface InteractionListener {
+        fun attachComponent(paint: IPaint)
+        fun detachComponent()
+    }
+}
