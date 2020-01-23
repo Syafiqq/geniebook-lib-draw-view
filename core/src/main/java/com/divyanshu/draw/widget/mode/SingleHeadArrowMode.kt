0895d778432bd07ae7c094f1562e01a173f1fa97
Syafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2020-01-23 14:37:29 +0700

    finishing draw single head arrow

--- core/src/main/java/com/divyanshu/draw/widget/mode/SingleHeadArrowMode.kt
+++ core/src/main/java/com/divyanshu/draw/widget/mode/SingleHeadArrowMode.kt
@@ -46,11 +46,12 @@ class SingleHeadArrowMode(override val mode: DrawingMode): SingleShapeMode(mode)
             endPivot.centerPoint(initX, initY, endX, endY)
             endPivot1.calculatePoint(endPivot, strokeWidth * 2, pr, pm)
             endPivot2.calculatePoint(endPivot, -strokeWidth * 2, pr, pm)
+            endPivot.calculatePoint(endPivot, MathUtil.positiveSignum(endX - initX) * strokeWidth * 4, MathUtil.calculateR(m), m)
         } else {
             endPivot.arrowHeadPivot(initX, initY, endX, endY, strokeWidth * 2, d)
             endPivot1.calculatePoint(endPivot, strokeWidth * 2, pr, pm)
             endPivot2.calculatePoint(endPivot, -strokeWidth * 2, pr, pm)
-            endPivot.calculatePoint(endPivot, strokeWidth * 4, MathUtil.calculateR(m), m)
+            endPivot.calculatePoint(endPivot, MathUtil.positiveSignum(endX - initX) * strokeWidth * 4, MathUtil.calculateR(m), m)
         }
 
         endPath.composePath(endPivot1, endPivot2, endPivot)
