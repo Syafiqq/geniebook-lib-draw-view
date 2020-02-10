e309812759381598b73888f76e653938419408eb
Syafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2020-02-10 17:22:17 +0700

    add convertToMap

--- core/src/main/java/com/divyanshu/draw/util/UnitConverter.kt
+++ core/src/main/java/com/divyanshu/draw/util/UnitConverter.kt
@@ -7,4 +7,12 @@ object UnitConverter {
     fun spToPx(context: Context, sp: Float): Float {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.resources.displayMetrics)
     }
+
+    fun <K, V, X> List<X>.convertToMap(key: (X) -> K, value: (X, i: Int) -> V): Map<K, V> {
+        val map = mutableMapOf<K, V>()
+        this.forEachIndexed { i, it ->
+            map[key(it)] = value(it, i)
+        }
+        return map
+    }
 }
